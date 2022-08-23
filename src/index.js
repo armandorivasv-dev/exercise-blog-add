3b57dfccbe5cfb66bb611e8052fde1008934f252
Armando Rivas <armandoweb.agency@gmail.com>
2022-08-23 15:28:47 -0400

    reactfire ok en master

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { FirebaseAppProvider } from 'reactfire';
 
 const firebaseConfig = {
@@ -24,4 +23,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
